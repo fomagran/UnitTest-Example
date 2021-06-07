9f33bdb5272415d29610d1342f2ab1195a3ab390
Fomagran <fomagran6@naver.com>
2021-06-07 15:59:11 +0900

    유닛 테스트 셋업과 티어다운은 매 테스트 마다 시작,끝과 호출된다. 테스트 이름에 따라 정렬되어 실행되기 때문에 어떤 순서로 놓아도 이름 순으로 실행됨.

--- UnitTest ExampleTests/UserModelTests.swift
+++ UnitTest ExampleTests/UserModelTests.swift
@@ -10,24 +10,28 @@ import XCTest
 
 class UserModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override class func setUp() {
+        print("set up")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override class func tearDown() {
+        print("tear dwon")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testD() {
+        print("test D")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testC() {
+        print("test C")
+    }
+    
+    func testB() {
+        print("test B")
+    }
+    
+    func testA() {
+        print("test A")
     }
 
 }
