04c04beaf39caaf972241a6276f59791b5a83b3d
Fomagran <fomagran6@naver.com>
2021-06-09 12:42:16 +0900

    프레젠터 테스트 리팩터링

--- UnitTest ExampleTests/PhotoApp/SignUp/Presenters/SignUpPresenterTests.swift
+++ UnitTest ExampleTests/PhotoApp/SignUp/Presenters/SignUpPresenterTests.swift
@@ -10,10 +10,20 @@ import XCTest
 
 class SignUpPresenterTests: XCTestCase {
     
+    var signUpModel:SignUpModel!
+    var mockSignUpModelValidator:MockSignUpModelValidator!
+    var mockSignUpWebService:MockSignUpWebService!
+    var sut:SignUpPresenter!
     
 
     override func setUp() {
+         signUpModel = SignUpModel(firstName:"Foma",lastName:"gran",email:"fomagran6@naver.com",password:"12345678",repeatPassword:"12345678")
         
+         mockSignUpModelValidator = MockSignUpModelValidator()
+        
+         mockSignUpWebService = MockSignUpWebService()
+        
+        sut = SignUpPresenter(validator:mockSignUpModelValidator,webservice: mockSignUpWebService)
     }
 
     override func tearDown() {
@@ -21,11 +31,6 @@ class SignUpPresenterTests: XCTestCase {
     }
     
     func testSignupPresenter_WhenInformationProvided_WillValidateEachProperty() {
-        let signUpModel = SignUpModel(firstName:"Foma",lastName:"gran",email:"fomagran6@naver.com",password:"12345678",repeatPassword:"12345678")
-        
-        let mockSignUpModelValidator = MockSignUpModelValidator()
-        let mockSignUpWebService = MockSignUpWebService()
-        let sut = SignUpPresenter(validator:mockSignUpModelValidator,webservice: mockSignUpWebService)
         
         sut.processUserSignUp(model:signUpModel)
         
@@ -35,13 +40,6 @@ class SignUpPresenterTests: XCTestCase {
     }
     
     func testSignupPresenter_WhenGivenValidModel_ShouldCallSignUpMmethod() {
-        let signUpModel = SignUpModel(firstName:"Foma",lastName:"gran",email:"fomagran6@naver.com",password:"12345678",repeatPassword:"12345678")
-        
-        let mockSignUpModelValidator = MockSignUpModelValidator()
-        
-        let mockSignUpWebService = MockSignUpWebService()
-        
-        let sut = SignUpPresenter(validator:mockSignUpModelValidator,webservice:mockSignUpWebService)
         
         sut.processUserSignUp(model:signUpModel)
         
