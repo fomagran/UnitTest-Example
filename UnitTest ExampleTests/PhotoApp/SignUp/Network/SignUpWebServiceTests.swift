c6cad43618a80f25f7932794929dfac821deaad1
Fomagran <fomagran6@naver.com>
2021-06-09 11:28:45 +0900

    SignUp 웹서비스 테스트 리팩토링

--- UnitTest ExampleTests/PhotoApp/SignUp/Network/SignUpWebServiceTests.swift
+++ UnitTest ExampleTests/PhotoApp/SignUp/Network/SignUpWebServiceTests.swift
@@ -10,27 +10,33 @@ import XCTest
 
 class SignUpWebServiceTests: XCTestCase {
     
+    var sut:SignUpWebService!
+    var signUpRequestModel:SignUpRequestModel!
 
     override func setUp() {
+        
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        let urlSession = URLSession(configuration: config)
+        
+        sut = SignUpWebService(urlString:SignUpConstants.signUpURLString,urlSession: urlSession)
+            
+        signUpRequestModel = SignUpRequestModel(firstName: "Foma", lastName: "gran", email: "fomagran6@naver.com", password: "12341234")
+    
         
     }
     
     override func tearDown() {
-        
+        sut = nil
+        signUpRequestModel = nil
+        MockURLProtocol.stubResponseData = nil
     }
     
     func testSignUpWebService_WhenGivenSuccessfulResponse_ReturnSuccess() {
-        
-        //TODO:이게 뭔지 알아보기
-        let config = URLSessionConfiguration.ephemeral
-        config.protocolClasses = [MockURLProtocol.self]
-        let urlSession = URLSession(configuration: config)
+ 
         let jsonString = "{\"status\":\"ok\"}"
         MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
         
-        let sut = SignUpWebService(urlString:SignUpConstants.signUpURLString,urlSession: urlSession)
-            
-        let signUpRequestModel = SignUpRequestModel(firstName: "Foma", lastName: "gran", email: "fomagran6@naver.com", password: "12341234")
         
         let expectation = self.expectation(description: "Signup web service response expectation")
 
@@ -45,22 +51,14 @@ class SignUpWebServiceTests: XCTestCase {
     
     func testSignUpWebService_WhenReceiveDifferentJSONResponse_ErrotTookPlace() {
         
-        let config = URLSessionConfiguration.ephemeral
-        config.protocolClasses = [MockURLProtocol.self]
-        let urlSession = URLSession(configuration: config)
         let jsonString = "{\"path\": \"/users\",\"error\":\"Internal Server Error\"}"
         MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
         
-        let sut = SignUpWebService(urlString:SignUpConstants.signUpURLString,urlSession: urlSession)
-            
-        let signUpRequestModel = SignUpRequestModel(firstName: "Foma", lastName: "gran", email: "fomagran6@naver.com", password: "12341234")
-        
-        let expectation = self.expectation(description: "Signup web service response expectation")
+        let expectation = self.expectation(description: "Signup() method expectation for a response that contains a different JSON structure")
 
         sut.signUp(with: signUpRequestModel) { response, error in
-            XCTAssertNil(response)
-            XCTAssertEqual(error, SignUpErrors.responseModelParsingError)
-            
+            XCTAssertNil(response,"The response model for a request containing unknown JSON response, should have been nil")
+            XCTAssertEqual(error, SignUpErrors.responseModelParsingError,"The signUp() method did not return expected error")
             expectation.fulfill()
         }
         
