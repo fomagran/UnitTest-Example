12bff967ffe679845c41a5bfec254a893cac5379
Fomagran <fomagran6@naver.com>
2021-07-20 10:24:13 +0900

    네비게이션 제대로 푸쉬됐는지 테스트하기

---
+++ UnitTest Example/PhotoApp/SignUp/Controllers/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  UnitTest Example
+//
+//  Created by Fomagran on 2021/07/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- UnitTest ExampleTests/PhotoApp/SignUp/Controllers/SignupViewControllerTests.swift
+++ UnitTest ExampleTests/PhotoApp/SignUp/Controllers/SignupViewControllerTests.swift
@@ -12,16 +12,19 @@ class SignupViewControllerTests: XCTestCase {
     
     var storyboard:UIStoryboard!
     var sut:SignupViewController!
+    var nav:UINavigationController!
 
     override func setUpWithError() throws {
         storyboard = UIStoryboard(name: "Main", bundle: nil)
         sut = storyboard.instantiateViewController(identifier: "SignupViewController") as SignupViewController
         //뷰를 로드해줘야 테스트를 할 수 있다.
         sut.loadViewIfNeeded()
+        nav = UINavigationController(rootViewController: sut)
     }
 
     override func tearDownWithError() throws {
-        
+        sut = nil
+        nav = nil
     }
     
     func testSignupViewController_WhenCreated_HasRequiredTextFieldsEmpty() throws {
@@ -62,6 +65,19 @@ class SignupViewControllerTests: XCTestCase {
     func testPasswordTextField_WhenCreated_HasSecureTextEntrySet() throws {
         let passwordTF = try XCTUnwrap(sut.passwordTF)
         XCTAssertTrue(passwordTF.isSecureTextEntry,"비밀번호는 SecureTextEntry로 설정되어야 하는데 그렇지 않네요")
+    }
+    
+    func testSignupButton_WhenTapped_PushMainViewController() {
+        
+        let myPredicate = NSPredicate { input, _ in
+            return (input as? UINavigationController)?.topViewController is MainViewController
+        }
+    
+        expectation(for: myPredicate, evaluatedWith: nav)
+        
+        sut.signupButton.sendActions(for: .touchUpInside)
+        
+        waitForExpectations(timeout: 1)
     }
 
 }
